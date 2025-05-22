156bcc831fbecb0d8ffbb035337de76f8304edc8
Peterson Prestes Carneiro <peters.prestes@gmail.com>
2025-05-22 18:11:39 -0300

    feat: code change

--- app/src/main/java/com/turnedoffmodem/MainActivity.java
+++ app/src/main/java/com/turnedoffmodem/MainActivity.java
@@ -1,65 +1,66 @@
 package com.turnedoffmodem;
 
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnTurnOff;
-    WebView webView;
 
+    WebView webView;
+    Button btnReboot;
+    String loginUrl = "http://192.168.1.254/";
+    String rebootUrl = "http://192.168.1.254/reboot.cgi";
+    boolean isLogged = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-        btnTurnOff = findViewById(R.id.btnTurnOff);
 
         webView = findViewById(R.id.webview);
+        btnReboot = findViewById(R.id.btnTurnOff);
+
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
 
-    }
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
 
+                if (url.equals(loginUrl) && !isLogged) {
+                    // Faz login
+                    webView.evaluateJavascript(
+                            "document.getElementById('username').value = 'userAdmin';" +
+                                    "document.getElementById('password').value = '3f68SPTDKp';" +
+                                    "document.getElementById('loginBT').click();", null
+                    );
 
-    public void login(View view) throws IOException {
-        String name = "userAdmin";
-        String password = "3f68SPTDKp";
+                    isLogged = true;
 
-        // Carrega a página de login do modem
-        webView.loadUrl("http://192.168.1.254/");
-
-        webView.evaluateJavascript(
-                "document.getElementById('WanIp') !== null",
-                value -> {
-                    if ("true".equals(value)) {
-                        Toast.makeText(MainActivity.this, "Login OK!", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(MainActivity.this, "Login FALHOU!", Toast.LENGTH_SHORT).show();
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
                     }
+
+                    webView.loadUrl(rebootUrl);
+                } else if (url.equals(rebootUrl)) {
+                    // Clica no botão de reboot
+                    webView.evaluateJavascript(
+                            "document.getElementById('do_reboot').click();", null
+                    );
+                } else {
+                    Toast.makeText(getApplicationContext(),"Entered the reboot url",Toast.LENGTH_LONG).show();
                 }
-        );
+            }
+        });
 
+        btnReboot.setOnClickListener(v -> {
+            isLogged = false;
+            webView.loadUrl(loginUrl);// Inicia processo
+        });
     }
-
-
-
 }
